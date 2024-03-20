e3ce3c26bfe19d2e855df79f3382f89e8cafe03f
Johnathonsb11 <153206172+Johnathonsb11@users.noreply.github.com>
2024-03-20 11:24:29 -0500

    Update Pronounce.js

--- Pronounce.js
+++ Pronounce.js
@@ -4,33 +4,27 @@ const border = pronunciation.style.borderWidth;
 const container = document.getElementById('container');
 const body = window.document.body;
 
-function shrink() {
+function shrink() { // animation to shrink the output box
     pronunciation.animate(
         [
             { transform: "scaleY(1)", borderWidth: "2px", borderLeftWidth: "2px", borderRightWidth: "2px"},
-            { transform: "scaleY(0)", borderWidth: "23px", borderLeftWidth: "2px", borderRightWidth: "2px"},
+            { transform: "scaleY(0.02)", borderWidth: "23px", borderLeftWidth: "2px", borderRightWidth: "2px"},
         ],
-        {
-            duration: 700,
-            easing: "cubic-bezier(0.7, 0.3, 0.2, 1)"
-        }
+        { duration: 700, easing: "cubic-bezier(0.7, 0.3, 0.2, 1)" }
     )
 }
 
-function grow() {
+function grow() { // animation to grow the output box back to normal
     pronunciation.animate(
         [
-            { transform: "scaleY(0)", borderWidth: "23px", borderLeftWidth: "2px", borderRightWidth: "2px"},
-            { transform: "scaleY(1)", borderWidth: "2px", borderLeftWidth: "2px", borderRightWidth: "2px"},
+            { transform: "scaleY(0.02)", borderWidth: "23px", borderLeftWidth: "2px", borderRightWidth: "2px"},
+            { transform: "scaleY(1)", borderWidth: "2px", borderLeftWidth: "2px", borderRightWidth: "2px"}, 
         ],
-        {
-            duration: 600,
-            easing: "cubic-bezier(0.7, 0.3, 0.2, 1)"
-        }
+        { duration: 600, easing: "cubic-bezier(0.7, 0.3, 0.2, 1)" }
     )
 }
 
-container.addEventListener("mousemove", function(e) {
+container.addEventListener("mousemove", function(e) { // makes the background move along with your cursor
     if (e.target === container) {
         body.style.backgroundPositionX = -e.offsetX * 0.05 + "px";
         body.style.backgroundPositionY = -e.offsetY * 0.05 + "px";      
@@ -57,138 +51,64 @@ numIn.addEventListener('keyup', (event) => {
             // hundreds placement
             if (inputValue.length >= 3) { // checks to see if the number is 3 or more digits
                 switch(inputValue[2]) { // takes the hundreds place as input
-                    case '1':
-                        finalPron += 'one hundred ';
-                        break;
-                    case '2':
-                        finalPron += 'two hundred ';
-                        break;
-                    case '3':
-                        finalPron += 'three hundred ';
-                        break;
-                    case '4':
-                        finalPron += 'four hundred ';
-                        break;
-                    case '5':
-                        finalPron += 'five hundred ';
-                        break;
-                    case '6':
-                        finalPron += 'six hundred ';
-                        break;
-                    case '7':
-                        finalPron += 'seven hundred ';
-                        break;
-                    case '8':
-                        finalPron += 'eight hundred ';
-                        break;
-                    case '9':
-                        finalPron += 'nine hundred ';
-                        break;
+                    case '1': finalPron += 'one hundred '; break;
+                    case '2': finalPron += 'two hundred '; break;
+                    case '3': finalPron += 'three hundred '; break;
+                    case '4': finalPron += 'four hundred '; break;
+                    case '5': finalPron += 'five hundred '; break;
+                    case '6': finalPron += 'six hundred '; break;
+                    case '7': finalPron += 'seven hundred '; break;
+                    case '8': finalPron += 'eight hundred '; break;
+                    case '9': finalPron += 'nine hundred '; break;
                 }
             }
 
             // teens
             if (inputValue.length >= 2) { // checks to see if the number is 2 or more digits
                 switch(inputValue[1] + inputValue [0]) { // takes the tens and ones places combined to get the teens
-                    case '10':
-                        finalPron += 'ten';
-                        break;
-                    case '11':
-                        finalPron += 'eleven';
-                        break;
-                    case '12':
-                        finalPron += 'twelve';
-                        break;
-                    case '13':
-                        finalPron += 'thirteen';
-                        break;
-                    case '14':
-                        finalPron += 'fourteen';
-                        break;
-                    case '15':
-                        finalPron += 'fifteen';
-                        break;
-                    case '16':
-                        finalPron += 'sixteen';
-                        break;
-                    case '17':
-                        finalPron += 'seventeen';
-                        break;
-                    case '18':
-                        finalPron += 'eighteen';
-                        break;
-                    case '19':
-                        finalPron += 'nineteen';
-                        break;
+                    case '10': finalPron += 'ten'; break;
+                    case '11': finalPron += 'eleven'; break;
+                    case '12': finalPron += 'twelve'; break;
+                    case '13': finalPron += 'thirteen'; break;
+                    case '14': finalPron += 'fourteen'; break;
+                    case '15': finalPron += 'fifteen'; break;
+                    case '16': finalPron += 'sixteen'; break;
+                    case '17': finalPron += 'seventeen'; break;
+                    case '18': finalPron += 'eighteen'; break;
+                    case '19': finalPron += 'nineteen'; break;
                 }
 
                 // tens placement
                 switch(inputValue[1]) { // takes the tens place as input
-                    case '2':
-                        finalPron += 'twenty ';
-                        break;
-                    case '3':
-                        finalPron += 'thirty ';
-                        break;
-                    case '4':
-                        finalPron += 'forty ';
-                        break;
-                    case '5':
-                        finalPron += 'fifty ';
-                        break;
-                    case '6':
-                        finalPron += 'sixty ';
-                        break;
-                    case '7':
-                        finalPron += 'seventy ';
-                        break;
-                    case '8':
-                        finalPron += 'eighty ';
-                        break;
-                    case '9':
-                        finalPron += 'ninety ';
-                        break;
+                    case '2': finalPron += 'twenty '; break;
+                    case '3': finalPron += 'thirty '; break;
+                    case '4': finalPron += 'forty '; break;
+                    case '5': finalPron += 'fifty '; break;
+                    case '6': finalPron += 'sixty '; break;
+                    case '7': finalPron += 'seventy '; break;
+                    case '8': finalPron += 'eighty '; break;
+                    case '9': finalPron += 'ninety '; break;
                 }
             }
 
             // ones placement
             if (inputValue.length >= 1 && inputValue[1] !== '1') { // checks to see if number is 1 or more digits and if the tens place is a one to properly output the teens
                 switch(inputValue[0]) {
-                    case '1':
-                        finalPron += 'one';
-                        break;
-                    case '2':
-                        finalPron += 'two';
-                        break;
-                    case '3':
-                        finalPron += 'three';
-                        break;
-                    case '4':
-                        finalPron += 'four';
-                        break;
-                    case '5':
-                        finalPron += 'five';
-                        break;
-                    case '6':
-                        finalPron += 'six';
-                        break;
-                    case '7':
-                        finalPron += 'seven';
-                        break;
-                    case '8':
-                        finalPron += 'eight';
-                        break;
-                    case '9':
-                        finalPron += 'nine';
-                        break;
+                    case '1': finalPron += 'one'; break;
+                    case '2': finalPron += 'two'; break;
+                    case '3': finalPron += 'three'; break;
+                    case '4': finalPron += 'four'; break;
+                    case '5': finalPron += 'five'; break;
+                    case '6': finalPron += 'six'; break;
+                    case '7': finalPron += 'seven'; break;
+                    case '8': finalPron += 'eight'; break;
+                    case '9': finalPron += 'nine'; break;
                 }
             }
 
             if (inputValue.length === 1) { // isolates zero to only appear when its by itself since thats really the only time you see it
                 switch(inputValue[0]) {
-                    case '0':
-                        finalPron += 'zero';
-                        break;
+                    case '0': finalPron += 'zero'; break;
                 }
             }
 
